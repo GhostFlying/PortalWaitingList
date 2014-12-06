1d05456718d5b756d0c11c1dae48ad3236348c2f
GhostFlying <GhostFlying@Gmail.com>
2014-12-06 21:45:12 +0800

    remove unused code

--- app/src/main/java/com/ghostflying/portalwaitinglist/Util/GMailServiceUtil.java
+++ app/src/main/java/com/ghostflying/portalwaitinglist/Util/GMailServiceUtil.java
@@ -40,16 +40,9 @@ import retrofit.RestAdapter;
 public class GMailServiceUtil {
     private static final long ONE_DAY_MILLISECONDS = 3600L * 24 * 1000;
     private static final String QUERY_STRING = "from:(super-ops@google.com OR ingress-support@google.com) subject:(Ingress Portal)";
-    private static final String QUERY_MESSAGE_FORMAT = "?format=metadata";
+    private static final String QUERY_MESSAGE_FORMAT = "?format=full";
     private static final String QUERY_MESSAGE_METADATA_DATE = "&metadataHeaders=date";
     private static final String QUERY_MESSAGE_METADATA_SUBJECT = "&metadataHeaders=subject";
-    //private static final String[] QUERY_MESSAGE_METADATA = {QUERY_MESSAGE_METADATA_SUBJECT, QUERY_MESSAGE_METADATA_DATE};
-//    private static final String REGEX_PORTAL_SUBMIT = "(?<=Ingress Portal Submitted:).+";
-//    private static final String REGEX_PORTAL_EDIT = "(?<=Ingress Portal Edits Submitted:).+|" + "(?<=Invalid Ingress Portal Report:).+";
-//    private static final String REGEX_PORTAL_SUBMIT_PASSED = "(?<=Ingress Portal Live:).+";
-//    private static final String REGEX_PORTAL_SUBMIT_REJECTED = "(?<=Ingress Portal Rejected:).+|" + "(?<=Ingress Portal Duplicate:).+";
-//    private static final String REGEX_PORTAL_EDIT_PASSED = "(?<=Ingress Portal Data Edit Accepted:).+";
-//    private static final String REGEX_PORTAL_EDIT_REJECTED = "(?<=Ingress Portal Data Edit Reviewed:).+";
     private static final String REGEX_EACH_JSON_IN_BATCH = "\\{.+\\}";
     private static final String REGEX_FIND_BOUNDARY = "(?<=boundary=).+";
     private static final String DEFAULT_AFTER_STR = "1995/07/08";
@@ -57,30 +50,17 @@ public class GMailServiceUtil {
     private static final String BATCH_REQUEST_URL = "https://www.googleapis.com/batch";
     private static final MediaType HTTP = MediaType.parse("application/http");
     private static GMailServiceUtil instance;
-    private String token;
-//    private Pattern patternSubmit;
-//    private Pattern patternEdit;
-//    private Pattern patternSubmitPassed;
-//    private Pattern patternSubmitRejected;
-//    private Pattern patternEditPassed;
-//    private Pattern patternEditRejected;
     private Pattern patternEachResponse;
     private Pattern patternBoundary;
     private Gson gson;
     private GMailService gmailService;
+    private String token;
 
     /**
-     * Private constructor
-     */
-    private GMailServiceUtil(){
-
-    }
-
-    /**
-     * Set the token before query.
+     * Private constructor.
      * @param token the token to query.
      */
-    public void setToken(final String token){
+    private GMailServiceUtil(final String token){
         RequestInterceptor requestInterceptor = new RequestInterceptor() {
             @Override
             public void intercept(RequestFacade request) {
@@ -100,9 +80,9 @@ public class GMailServiceUtil {
      * Get an instance of GMailServiceUtil.
      * @return the instance.
      */
-    public static GMailServiceUtil getInstance(){
+    public static GMailServiceUtil getInstance(final String token){
         if (instance == null){
-            instance = new GMailServiceUtil();
+            instance = new GMailServiceUtil(token);
         }
         return instance;
     }
@@ -257,254 +237,4 @@ public class GMailServiceUtil {
             existEvent.close();
         }
     }
-
-//    /**
-//     * Convert messages to events.
-//     * @param messages the origin messages.
-//     * @return the converted events.
-//     */
-//    public ArrayList<PortalEvent> analysisMessages(ArrayList<Message> messages){
-//        ArrayList<PortalEvent> portalEvents = new ArrayList<PortalEvent>();
-//
-//
-//        for (Message message : messages){
-//            PortalEvent portalEvent = analysisMessage(message);
-//            if (portalEvent != null)
-//                portalEvents.add(portalEvent);
-//        }
-//        return portalEvents;
-//    }
-
-//    /**
-//     * Convert message to event.
-//     * @param message the origin message.
-//     * @return the converted event.
-//     */
-//    private PortalEvent analysisMessage(Message message){
-//        //Initial pattern
-//        if (patternSubmit == null){
-//            patternSubmit = Pattern.compile(REGEX_PORTAL_SUBMIT);
-//            patternEdit = Pattern.compile(REGEX_PORTAL_EDIT);
-//            patternSubmitPassed = Pattern.compile(REGEX_PORTAL_SUBMIT_PASSED);
-//            patternSubmitRejected = Pattern.compile(REGEX_PORTAL_SUBMIT_REJECTED);
-//            patternEditPassed = Pattern.compile(REGEX_PORTAL_EDIT_PASSED);
-//            patternEditRejected = Pattern.compile(REGEX_PORTAL_EDIT_REJECTED);
-//        }
-//
-//        Matcher matcher;
-//
-//        // Try to match regex.
-//        matcher = patternSubmit.matcher(message.getSubject());
-//        if (matcher.find()){
-//            return new SubmissionEvent(matcher.group().trim(),
-//                    PortalEvent.OperationResult.PROPOSED, message.getDate(), message.getId(), null);
-//        }
-//
-//        matcher = patternEdit.matcher(message.getSubject());
-//        if (matcher.find()){
-//            return new PortalEvent(matcher.group().trim(), PortalEvent.OperationType.EDIT,
-//                    PortalEvent.OperationResult.PROPOSED, message.getDate(), message.getId());
-//        }
-//
-//        matcher = patternSubmitPassed.matcher(message.getSubject());
-//        if (matcher.find()){
-//            return new PortalEvent(matcher.group().trim(), PortalEvent.OperationType.SUBMIT,
-//                    PortalEvent.OperationResult.PASSED, message.getDate(), message.getId());
-//        }
-//
-//        matcher = patternSubmitRejected.matcher(message.getSubject());
-//        if (matcher.find()){
-//            return new PortalEvent(matcher.group().trim(), PortalEvent.OperationType.SUBMIT,
-//                    PortalEvent.OperationResult.REJECTED, message.getDate(), message.getId());
-//        }
-//
-//        matcher = patternEditPassed.matcher(message.getSubject());
-//        if (matcher.find()){
-//            return new PortalEvent(matcher.group().trim(), PortalEvent.OperationType.EDIT,
-//                    PortalEvent.OperationResult.PASSED, message.getDate(), message.getId());
-//        }
-//
-//        matcher = patternEditRejected.matcher(message.getSubject());
-//        if (matcher.find()){
-//            return new PortalEvent(matcher.group().trim(), PortalEvent.OperationType.EDIT,
-//                    PortalEvent.OperationResult.REJECTED, message.getDate(), message.getId());
-//        }
-//
-//        return null;
-//    }
-
-//    /**
-//     * Merge new events to exist portal detail or create new one.
-//     * @param origin exist portal detail list.
-//     * @param events events need to be merged.
-//     * @return the merged portal detail list.
-//     */
-//    public ArrayList<PortalDetail> mergeEvents(ArrayList<PortalDetail> origin, ArrayList<PortalEvent> events){
-//        for (PortalEvent event : events){
-//            PortalDetail existDetail = findExistDetail(origin, event.getPortalName());
-//            if (existDetail == null){
-//                PortalDetail newDetail = new PortalDetail(event);
-//                origin.add(newDetail);
-//            }
-//            else {
-//                existDetail.addEvent(event);
-//                // sort the event list.
-//                Collections.sort(existDetail.getEvents());
-//            }
-//        }
-//        return origin;
-//    }
-//
-//    /**
-//     * Find the exist portal detail has the same name with new event name
-//     * @param details   exist portal detail list.
-//     * @param eventName new event name.
-//     * @return  the portal detail if exists, or null if it does not exist.
-//     */
-//    private PortalDetail findExistDetail(ArrayList<PortalDetail> details, String eventName){
-//        for (PortalDetail detail : details){
-//            if (detail.getName().equals(eventName))
-//                return detail;
-//        }
-//        return null;
-//    }
-//
-//
-//    /**
-//     * To sort and filter origin and store result to edit.
-//     * @param filterMethod  the filter method to use.
-//     * @param sortOrder     the sort order to use.
-//     * @param origin        the origin list.
-//     * @param edit          the result list.
-//     */
-//    public void filterAndSort(SettingUtil.FilterMethod filterMethod,
-//                              SettingUtil.SortOrder sortOrder,
-//                              ArrayList<PortalDetail> origin,
-//                              ArrayList<PortalDetail> edit){
-//        filterPortalDetails(filterMethod, origin, edit);
-//        sortPortalDetails(sortOrder, edit);
-//    }
-//
-//    /**
-//     * Filter the list of portal details.
-//     * @param filterMethod  the filter method to use.
-//     * @param origin        the origin list to be filter.
-//     * @param filtered      the filtered list.
-//     */
-//    private void filterPortalDetails(SettingUtil.FilterMethod filterMethod,
-//                                    ArrayList<PortalDetail> origin,
-//                                    ArrayList<PortalDetail> filtered){
-//        DoCheck doCheck;
-//
-//        switch (filterMethod){
-//            case WAITING:
-//                doCheck = new DoWaitingCheck();
-//                break;
-//            case ACCEPTED:
-//                doCheck = new DoAcceptedCheck();
-//                break;
-//            case REJECTED:
-//                doCheck = new DoRejectedCheck();
-//                break;
-//            case EVERYTHING:
-//            default:
-//                doCheck = new DoEveryThingCheck();
-//        }
-//
-//        filtered.clear();
-//
-//        for (PortalDetail detail : origin){
-//            if (doCheck.checkFilterMethod(detail))
-//                filtered.add(detail);
-//        }
-//    }
-//
-//    /**
-//     * Sort the list of portal details.
-//     * @param sortOrder     the order set.
-//     * @param portalDetails the sorted list.
-//     */
-//    private void sortPortalDetails(SettingUtil.SortOrder sortOrder, ArrayList<PortalDetail> portalDetails){
-//        switch (sortOrder){
-//            case DATE_ASC:
-//                Collections.sort(portalDetails);
-//                break;
-//            case DATE_DESC:
-//                Collections.sort(portalDetails, Collections.reverseOrder());
-//                break;
-//            case SMART_ORDER:
-//                Collections.sort(portalDetails, new Comparator<PortalDetail>() {
-//                    @Override
-//                    public int compare(PortalDetail lhs, PortalDetail rhs) {
-//                        if (!(lhs.isReviewedOrNoResponseForLongTime() ^ rhs.isReviewedOrNoResponseForLongTime()))
-//                            return lhs.compareTo(rhs);
-//                        else{
-//                            if (lhs.isReviewedOrNoResponseForLongTime())
-//                                return 1;
-//                            else
-//                                return -1;
-//                        }
-//                    }
-//                });
-//        }
-//    }
-//
-//
-//    /**
-//     * Get the accepted count and
-//     * @param details   the total details.
-//     * @return          the accepted count and the rejected count. the item 0 is the accepted count,
-//     *                  the item 1 is the rejected count.
-//     */
-//    public int[] getCounts(ArrayList<PortalDetail> details) {
-//        int[] counts = new int[2];
-//        counts[0] = 0;
-//        counts[1] = 0;
-//        for (PortalDetail detail : details){
-//            if (detail.isAccepted())
-//                counts[0]++;
-//            else if (detail.isRejected())
-//                counts[1]++;
-//        }
-//        return counts;
-//    }
-//
-//    private abstract class DoCheck {
-//        public abstract boolean checkFilterMethod(PortalDetail detail);
-//    }
-//
-//    /**
-//     * a list of classes to do check in filter
-//     */
-//    private class DoEveryThingCheck extends DoCheck {
-//
-//        @Override
-//        public boolean checkFilterMethod(PortalDetail detail) {
-//            return true;
-//        }
-//    }
-//
-//    private class DoAcceptedCheck extends DoCheck {
-//
-//        @Override
-//        public boolean checkFilterMethod(PortalDetail detail) {
-//            return detail.isAccepted();
-//        }
-//    }
-//
-//    private class DoRejectedCheck extends DoCheck {
-//
-//        @Override
-//        public boolean checkFilterMethod(PortalDetail detail) {
-//            return detail.isRejected();
-//        }
-//    }
-//
-//    private class DoWaitingCheck extends DoCheck{
-//
-//        @Override
-//        public boolean checkFilterMethod(PortalDetail detail) {
-//            return !detail.isReviewed();
-//        }
-//    }
 }
