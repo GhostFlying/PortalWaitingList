9fb4614dc6fea3db59bc7f55fd3a08c52fb94e77
GhostFlying <GhostFlying@Gmail.com>
2015-01-14 21:42:01 +0800

    add content provider

---
+++ app/src/main/java/com/ghostflying/portalwaitinglist/dao/dbinfo/PortalEventDbInfo.java
@@ -0,0 +1,18 @@
+package com.ghostflying.portalwaitinglist.dao.dbinfo;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ghostflying on 1/14/15.
+ */
+public class PortalEventDbInfo implements BaseColumns {
+    public static final String TABLE_NAME = "event";
+    public static final String COLUMN_NAME_PORTAL_NAME = "portalname";
+    public static final String COLUMN_NAME_OPERATION_TYPE = "operationtype";
+    public static final String COLUMN_NAME_OPERATION_RESULT = "operationresult";
+    public static final String COLUMN_NAME_MESSAGE_ID = "messageid";
+    public static final String COLUMN_NAME_DATE = "date";
+    public static final String COLUMN_NAME_IMAGE_URL = "imageurl";
+    public static final String COLUMN_NAME_ADDRESS = "address";
+    public static final String COLUMN_NAME_ADDRESS_URL = "addressurl";
+}
