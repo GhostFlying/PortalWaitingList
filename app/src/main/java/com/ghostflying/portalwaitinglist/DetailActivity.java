abe03771b7261fd534bfbd13354f8430b5a9e3a7
GhostFlying <GhostFlying@Gmail.com>
2015-02-01 16:06:29 +0800

    Revert "return to set transition in code to boost performance"
    This reverts commit c9aef2c601a0d2fc78982acbe1a1c80328d9aeb5.

--- app/src/main/java/com/ghostflying/portalwaitinglist/DetailActivity.java
+++ app/src/main/java/com/ghostflying/portalwaitinglist/DetailActivity.java
@@ -1,14 +1,10 @@
 package com.ghostflying.portalwaitinglist;
 
 import android.app.Fragment;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
-import android.transition.ChangeBounds;
-import android.transition.TransitionSet;
 
-import com.ghostflying.portalwaitinglist.animation.PortalHeaderBackgroundTransition;
 import com.ghostflying.portalwaitinglist.fragment.ReDesignDetailFragment;
 import com.ghostflying.portalwaitinglist.util.SettingUtil;
 
@@ -20,22 +16,6 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // animation
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            TransitionSet enterSet = new TransitionSet();
-            enterSet.setOrdering(TransitionSet.ORDERING_TOGETHER);
-            enterSet.addTransition(new ChangeBounds());
-            PortalHeaderBackgroundTransition headerTransition = new PortalHeaderBackgroundTransition();
-            headerTransition.setMode(0);
-            enterSet.addTransition(headerTransition);
-            getWindow().setSharedElementEnterTransition(enterSet);
-            TransitionSet returnSet = new TransitionSet();
-            returnSet.addTransition(new ChangeBounds());
-            headerTransition = new PortalHeaderBackgroundTransition();
-            headerTransition.setMode(1);
-            returnSet.addTransition(headerTransition);
-            getWindow().setSharedElementReturnTransition(returnSet);
-        }
         setContentView(R.layout.activity_detail);
         Parcelable clickPortal = getIntent().getParcelableExtra(ARG_CLICKED_PORTAL);
         if (savedInstanceState == null) {
